c68c107d1ff1396c1ea7bc1f962a75f7370ed3ea
rehmanm <muhammad.rehman@ibexglobal.com>
2017-09-30 00:44:53 +0500

    model created

--- djangular/djangular/scrumboard/models.py
+++ djangular/djangular/scrumboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+ class List(models.Model):
+     name = models.CharField(max_length = 50)
+
+     def __str__(self):
+         return "List: {}".format(self.name)
+
+
+class Card(models.Model):
+    title = models.CharField(max_length = 100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return "Card: {}".format(self.title)
