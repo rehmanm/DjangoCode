bfa31ba5c7280fd0a6480809b67cf30bb6089cbe
rehmanm <muhammad.rehman@ibexglobal.com>
2017-09-30 00:53:33 +0500

    Added Foreign Key

--- djangular/djangular/scrumboard/models.py
+++ djangular/djangular/scrumboard/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
- class List(models.Model):
+class List(models.Model):
      name = models.CharField(max_length = 50)
 
      def __str__(self):
@@ -12,6 +12,10 @@ from django.db import models
 class Card(models.Model):
     title = models.CharField(max_length = 100)
     description = models.TextField(blank=True)
+    list = models.ForeignKey(List, related_name='cards')
+    story_points = models.IntegerField(null=True, blank=True)
+    business_value = models.IntegerField(null=True, blank=True)
+    
 
     def __str__(self):
         return "Card: {}".format(self.title)
